892886aeda45a3b6b994021da5aba58fa55809d6
Maryia.Vaitovich <Maryia.Vaitovich@WSA-050-73>
2023-04-16 23:44:26 +0300

    feat: add getDNSStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const separator = '.';
+  const result = {};
+
+  for(let domain of domains){
+    const slitDomain = domain.split(separator).reverse();
+    for(let index = 0; index < slitDomain.length; index++){
+      const resultDomain = `${separator}${slitDomain.slice(0, index +1).join(separator)}`;
+      result[resultDomain] ? result[resultDomain]++ : result[resultDomain] = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
