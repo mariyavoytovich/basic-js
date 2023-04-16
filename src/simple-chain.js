588282d57f502898622235e039fb36298cf389df
Maryia.Vaitovich <Maryia.Vaitovich@WSA-050-73>
2023-04-16 13:48:00 +0300

    feat: add chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,41 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  _chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this._chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if(typeof position !== 'number' || position <= 0 || position > this._chain.length)
+    {
+      this._chain = [];
+      throw new Error("You can\'t remove incorrect link!");
+    }
+
+    const index = position - 1;
+    this._chain.splice(index, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this._chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const resultChain = this._chain.map(x => `( ${x} )`).join('~~');
+    this._chain = [];
+    return resultChain;
   }
 };
 
